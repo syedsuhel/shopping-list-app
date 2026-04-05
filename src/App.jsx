3c4f69e07431465ed4f3fa5fb8810208c5c97800
syedsuhel <syedsuhel2624@gamil.com>
2026-04-05 14:14:16 +0530

    navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import Home from './Pages/Home'
 import { Route, Routes } from 'react-router-dom'
 import About from './Pages/About'
 import Contact from './Pages/Contact'
+import Navbar from './Component/Navbar'
 
 function App() {
   return (
     <>
+    <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
+      <Route path="/aboutus" element={<About />} />
+      <Route path="/contactus" element={<Contact />} />
     </Routes>
     </>
   )
